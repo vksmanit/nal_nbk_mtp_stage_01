51813012ba391b9b70ad3c371cf72b183d01067c
vksmanit <vksmanit@gmail.com>
2018-03-13 18:42:20 +0530

    sync commit

--- nal_nbk_conductance_matrix_for_NAL.m
+++ nal_nbk_conductance_matrix_for_NAL.m
@@ -1,6 +1,6 @@
-function conductace_matrix_for_NAL = nal_nbk_conductace_matrix_for_NAL(cktnetlist)
+function conductance_matrix_for_NAL = nal_nbk_conductance_matrix_for_NAL(cktnetlist)
 % --------------------------------------------------------------------------------
-% Syntax :- conductace_matrix_for_NAL = nal_nbk_conductace_matrix_for_NAL(cktnetlist)
+% Syntax :- conductance_matrix_for_NAL = nal_nbk_conductance_matrix_for_NAL(cktnetlist)
 %
 % This function will return conductace_matrix_for_NAL of for network NAL i.e. graph Gx(AUL).
 % --------------------------------------------------------------------------------
@@ -8,12 +8,14 @@ function conductace_matrix_for_NAL = nal_nbk_conductace_matrix_for_NAL(cktnetlis
 % ----------------------------- written on : Mar 13, 2018 ------------------------- 
 
      nal_nbk_A_and_B_part = nal_nbk_partition(cktnetlist) ;
-     G = zeros(max(find(nal_nbk_A_and_B_part)));
+     conductance_matrix_for_NAL = zeros(max(find(nal_nbk_A_and_B_part)));
     
-     for i = 1:length(G)
+     for i = 1:length(conductance_matrix_for_NAL)
          if (strcmp(cktnetlist.elements{i}.name(1),'R'))
              resistance_value = cell2mat(cktnetlist.elements{i}.parms);
-             G(i,i) = 1/resistance_value;
+             conductance_matrix_for_NAL (i,i) = 1/resistance_value;
          end
      end
+
+     %EA 
 end
--- nal_nbk_incedence_matrix_for_NAL.m
+++ nal_nbk_incedence_matrix_for_NAL.m
@@ -10,10 +10,10 @@ function  incedence_matrix_for_NAL =  nal_nbk_incedence_matrix_for_NAL(cktnetlis
     
     modified_edges_for_NAL = nal_nbk_modified_edges_for_NAL_graph(cktnetlist) ;
     [nodeVisited,edgeId_of_tree_of_A,dfs_nodes_of_A]= nal_nbk_dfs_search_of_G_dot_A(cktnetlist);
-    incedence_matrix = zeros(length(dfs_nodes_of_A),size(modified_edges_for_NAL,1));
+    incedence_matrix_for_NAL = zeros(length(dfs_nodes_of_A),size(modified_edges_for_NAL,1));
     
     for k = 1:size(modified_edges_for_NAL,1)
-        A(modified_edges_for_NAL(k,1),k) = 1;
-        A(modified_edges_for_NAL(k,2),k) = -1;
+        incedence_matrix_for_NAL (modified_edges_for_NAL(k,1),k) = 1;
+        incedence_matrix_for_NAL (modified_edges_for_NAL(k,2),k) = -1;
     end
 end
