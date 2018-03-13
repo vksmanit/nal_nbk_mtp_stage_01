1b9014b267c486e4e79f16b03d6e93cfb6c912f2
vksmanit <vksmanit@gmail.com>
2018-03-13 12:55:13 +0530

    commited with nal_nbk_incedence_matrix_for_NAL, giving correct incedence matrix for nondisconnected graph

---
+++ nal_nbk_incedence_matrix_for_NAL.m
@@ -0,0 +1,19 @@
+function A =  nal_nbk_incedence_matrix_for_NAL(cktnetlist)
+% --------------------------------------------------------------------------------
+% syntax :- incdence_matrix_for_NAL = nal_nbk_incedence_matrix_for_NAL(cktnetlist)
+%
+% This function will return incedence matrix of for network NAL i.e. graph Gx(AUL).
+% --------------------------------------------------------------------------------
+
+% -------------------------- written on : Mar 13, 2018 ---------------------------
+
+    
+    modified_edges_for_NAL = nal_nbk_modified_edges_for_NAL_graph(cktnetlist) ;
+    [nodeVisited,edgeId_of_tree_of_A,dfs_nodes_of_A]= nal_nbk_dfs_search_of_G_dot_A(cktnetlist);
+    incedence_matrix = zeros(length(dfs_nodes_of_A),size(modified_edges_for_NAL,1));
+    
+    for k = 1:size(modified_edges_for_NAL,1)
+        A(modified_edges_for_NAL(k,1),k) = 1;
+        A(modified_edges_for_NAL(k,2),k) = -1;
+    end
+end
